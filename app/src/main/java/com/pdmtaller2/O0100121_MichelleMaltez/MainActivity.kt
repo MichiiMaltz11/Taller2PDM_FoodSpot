61c41628151dc846f00e4d6e67aa113b30bac51a
MichiiMaltz11 <00100121@uca.edu.sv>
2025-04-30 13:18:29 -0600

    Commit inicial de proyecto en blanco

---
+++ app/src/main/java/com/pdmtaller2/O0100121_MichelleMaltez/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.pdmtaller2.O0100121_MichelleMaltez
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pdmtaller2.O0100121_MichelleMaltez.ui.theme.FoodSpotByMaltezTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodSpotByMaltezTheme {
+
+            }
+        }
+    }
+}
